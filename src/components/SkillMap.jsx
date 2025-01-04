35120fd31e7bc8a8b8b564cd64ffab90af7f6f07
Burak-Salca <salcaburak@gmail.com>
2025-01-04 18:08:03 +0300

    Yetenkler kısmında imleç açıklması eklendi

--- src/components/SkillMap.jsx
+++ src/components/SkillMap.jsx
@@ -1,10 +1,15 @@
 import React from 'react'
 
 export default function SkillMap(props) {
-   const {title,image} = props;
+   const {title, image} = props;
   return (
-    <div className="flex flex-col items-center text-center  ">
-        <img className='w-[75px] h-[75px] object-contain' src={image} />
+    <div className="flex flex-col items-center text-center">
+        <img 
+          className='w-[75px] h-[75px] object-contain' 
+          src={image} 
+          alt={title} 
+          title={title} 
+        />
     </div>
   )
 }
